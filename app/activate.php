65f66e530feea61585479caec251d2928508110b
Jason Agnew <jason@bigbitecreative.com>
2015-05-28 23:26:43 +0100

    Doc blocks

--- app/activate.php
+++ app/activate.php
@@ -1,2 +1,9 @@
 <?php
 
+/** @var  \Herbert\Framework\Application $container */
+/** @var  \Herbert\Framework\Http $http */
+/** @var  \Herbert\Framework\Router $router */
+/** @var  \Herbert\Framework\Enqueue $enqueue */
+/** @var  \Herbert\Framework\Panel $panel */
+/** @var  \Herbert\Framework\Shortcode $shortcode */
+/** @var  \Herbert\Framework\Widget $widget */
