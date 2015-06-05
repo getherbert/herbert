a2c4f8df33c9b1a5efebef2c3abb7a34b428a442
Jason Agnew <j@agnew.co>
2015-06-05 17:10:47 +0100

    Have access to Helper class from twig

--- app/api.php
+++ app/api.php
@@ -1,3 +1,15 @@
 <?php namespace MyPlugin;
 
 /** @var \Herbert\Framework\API $api */
+
+/**
+ * Gives you access to the Helper class from Twig
+ * {{ MyPlugin.helper('assetUrl', 'icon.png') }}
+ */
+$api->add('helper', function ()
+{
+    $args = func_get_args();
+    $method = array_shift($args);
+
+    return forward_static_call_array(__NAMESPACE__ . '\\Helper::' . $method, $args);
+});
